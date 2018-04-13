cb86a0b268c991c62b1eb052a481cb85873018d1
sinneren <sinneren@gmail.com>
2018-04-13 18:13:18 +0500

    err msg

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -2,15 +2,42 @@ import React, { Component } from 'react';
 import './index.css';
 
 export default class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.loginInput = React.createRef();
+        this.passInput = React.createRef();
+        this.state = {
+            err: false
+        }
+    }
+    onLoginBtnHandler(event) {
+        let data = {
+            name: this.loginInput.current.value,
+            password: this.passInput.current.value
+        }
+        if (data.name.toLowerCase() === 'admin' && data.password === '12345') {
+            this.setState({
+                err: false
+            });
+        } else {
+            this.setState({
+                err: true
+            });
+        }
+    }
     render() {
+        const errMsg = <div className="col err-txt">
+            <p>Имя пользователя или пароль введены не верно </p>
+        </div>;
         return (
             <div className="form">
                 <h2>Авторизация</h2>
                 <div className="col">
-                    <input type="text" placeholder="Login" className="form-control" />
-                    <input type="password" placeholder="Password" className="form-control" />
-                    <button className="btn">Enter</button>
+                    <input type="text" ref={this.loginInput} placeholder="Login" className="form-control" />
+                    <input type="password" ref={this.passInput} placeholder="Password" className="form-control" />
+                    <button className="btn" onClick={this.onLoginBtnHandler.bind(this)}>Enter</button>
                 </div>
+                {this.state.err ? errMsg : ''}
             </div>
         )
     }
